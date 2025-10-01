6c91a9a857d228772f845d67046b76b68cd470d1
JhogertBita <141960659+JhogertBita@users.noreply.github.com>
2025-10-01 10:28:14 -0500

    corección

--- homework/queries.py
+++ homework/queries.py
@@ -4,63 +4,6 @@
 # pylint: disable=import-error
 
 from homework.mapreduce import mapreduce  # type: ignore
-
-#
-# Columns:
-# total_bill, tip, sex, smoker, day, time, size
-#
-
-#
-# QUERY 1:
-# SELECT *, tip/total_bill as tip_rate
-# FROM tips;
-#
-
-def mapper_query_1(sequence):
-    """Mapper"""
-    result = []
-    for index, (_, row) in enumerate(sequence):
-        if index == 0:
-            result.append(
-                (index, row.strip() + ",tip_rate")
-            )
-        else:
-            row_values = row.strip().split(",")
-            total_bill = float(row_values[0])
-            tip = float(row_values[1])
-            tip_rate = tip / total_bill
-            result.append((index, row.strip() + "," + str(tip_rate)))
-    return result
-
-
-def reducer_query_1(sequence):
-    """Reducer"""
-    return sequence
-
-
-#
-# ORQUESTADOR:
-#
-def run():
-    """Orquestador"""
-
-    mapreduce(
-        input_folder="files/input/", 
-        output_folder="files/query_1", 
-        mapper_fn=mapper_query_1, 
-        reducer_fn=reducer_query_1,
-    )
-
-
-if __name__ == "__main__":
-
-    run()
-"""Taller evaluable"""
-
-# pylint: disable=broad-exception-raised
-# pylint: disable=import-error
-
-from homework.mapreduce import mapreduce  # type: ignore
 import shutil
 import os
 #
@@ -164,8 +107,6 @@ def reducer_query_4(sequence):
     """Reducer"""
     return sequence
 
-
-
 #
 # SELECT sex, count(*)
 # FROM tips
@@ -190,8 +131,6 @@ def reducer_query_5(sequence):
             counter[key] = 0
         counter[key] += value
     return list(counter.items())
-
-
 
 #
 # ORQUESTADOR:
@@ -251,6 +190,6 @@ def run():
         reducer_fn=reducer_query_5,
     )    
 
+
 if __name__ == "__main__":
-  
-  run()
+    run()
